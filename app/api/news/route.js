75f218c8d57c7c8f260a249f9a30d12c5ff84d6e
sergio088 <ssergiosantos088@gmail.com>
2025-11-24 16:07:29 -0300

    ARRUMANDO API

--- app/api/news/route.js
+++ app/api/news/route.js
@@ -1,18 +1,5 @@
-import { promises as fs } from "fs";
-import path from "path";
+import news from "@/public/news.json";
 
 export async function GET() {
-  try {
-    const filePath = path.join(process.cwd(), "public", "news.json");
-    const content = await fs.readFile(filePath, "utf8");
-    const news = JSON.parse(content);
-
-    return Response.json({ success: true, data: news });
-  } catch (err) {
-    console.error(err);
-    return Response.json(
-      { success: false, error: "Erro ao ler notícias" },
-      { status: 500 }
-    );
-  }
+  return Response.json({ success: true, data: news });
 }
